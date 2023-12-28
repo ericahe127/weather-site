03eff0a1cc1db6cbdf93a8546ebc08d1d2eda9d4
ericahe127 -Git <hexuan426@gmail.com>
2023-12-28 22:42:42 +0800

    get it to work

--- index.js
+++ index.js
@@ -7,13 +7,37 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var key = "dd0a7e0038b339db4d7dc345d05cfca7";
-var url = "https://api.openweathermap.org/data/3.0/onecall?";
+var url = "https://api.openweathermap.org/data/2.5/weather";
 var units = "metric";
 
 
 app.get("/", (req,res)=>{
     res.render("index.ejs", { content: "Waiting for data..." });
 });
+
+app.post("/get", async (req,res)=>{
+    const lat = req.body.lat;
+    console.log(lat);
+    const lon = req.body.lon;
+    console.log(lon);
+    try {
+    const result = await axios.get(url , {
+        params : {
+            appid: key,
+            lat : lat,
+            lon: lon,
+            units: units,
+        },
+    });
+    // console.log(JSON.stringify(result.data));
+    console.log(result.data);
+    res.render("index.ejs", {data: result.data});
+
+  } catch (error) {
+    console.log(JSON.stringify(error.response.data))
+    res.render("index.ejs", { data: JSON.stringify(error.response.data) });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
